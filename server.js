674af29a4fee19186e707773f434a70a1cf1795f
mohamedmasseye <mohamed.masseye@gmail.com>
2025-12-17 17:15:38 +0000

    Fix server

--- server.js
+++ server.js
@@ -133,7 +133,12 @@ const corsOptions = {
 };
 
 app.use(helmet({ crossOriginResourcePolicy: false }));
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
+  credentials: true
+}));
 app.use(express.json());
 app.use('/uploads', express.static(uploadDir));
 
